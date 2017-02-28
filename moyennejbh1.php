0527970787aea8889d49ac5a0f7f8fbacf4033a9
Jean-Baptiste Hallassou <jb.hallassou@gmail.com>
2017-02-28 22:45:25 +0100

    nouvel modif

---
+++ moyennejbh1.php
@@ -0,0 +1,11 @@
+<?php
+            $students = [
+                "Emmanuel"  => 42,
+                "Thierry"   => 51,
+                "Pascal"    => 45,
+                "Eric"      => 48,
+                "Nicolas"   => 19
+            ];
+            var_dump($students);
+            echo  array_sum($students)/ count($students) . "\n";
+?>
